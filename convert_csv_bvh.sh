d879a6b22edb8a086597d7c0839ded8ed59a7c76
ManifoldFR <manifoldfr@outlook.com>
2020-01-14 20:07:30 +0100

    set json_to_bvh to use deepdish to load h5 (yes...)

--- convert_csv_bvh.sh
+++ convert_csv_bvh.sh
@@ -1,3 +1,4 @@
 python json_to_bvh.py
-blender --background csv_to_bvh.blend -noaudio -P csv_to_bvh.py
+# blender --background csv_to_bvh.blend -noaudio -P csv_to_bvh.py
+blender csv_to_bvh.blend -noaudio -P csv_to_bvh.py
 python convert_bvh_mimic.py
--- json_to_bvh.py
+++ json_to_bvh.py
@@ -1,4 +1,5 @@
-import json, os, glob
+import os, glob
+import deepdish as dd
 import numpy as np
 import pandas as pd 
 
@@ -12,13 +13,12 @@ def join_csv():
     concatenated_df['frame'] = concatenated_df.index+1
     concatenated_df.to_csv("bvh/csv_joined.csv", index=False)
 
-file_name = 'refined/hammer.json'
+file_name = 'refined/walken-1.h5'
+data = dd.io.load(os.path.join(os.getcwd(), file_name))
 
-with open(os.path.join(os.getcwd(), file_name)) as f:
-    data = json.load(f)
 
-keys = [int(k) for k in data]
-keys = sorted(keys)
+keys = data.keys()
+
 
 joints_names = ['Ankle.R_x', 'Ankle.R_y', 'Ankle.R_z',
             'Knee.R_x', 'Knee.R_y', 'Knee.R_z',
@@ -41,8 +41,10 @@ joints_names = ['Ankle.R_x', 'Ankle.R_y', 'Ankle.R_z',
             'Ear.R_x', 'Ear.R_y', 'Ear.R_z']
 
 for k in keys:
-    print("Frame %d: joints3d shape %s" % (k, str(np.array(data[str(k)]['joints3d']).shape)))
-    joints_export = pd.DataFrame(np.array(data[str(k)]['joints3d']).reshape(1,57), columns=joints_names)
+    item = data[k][0]
+    joints3d = np.asarray(item['joints3d'])
+    print("Frame %d: joints3d shape %s" % (k, str(joints3d.shape)))
+    joints_export = pd.DataFrame(joints3d.reshape(1,57), columns=joints_names)
     joints_export.drop(['Nose_x', 'Nose_y', 'Nose_z', 
             'Eye.L_x', 'Eye.L_y', 'Eye.L_z', 
             'Eye.R_x', 'Eye.R_y', 'Eye.R_z', 
@@ -52,12 +54,34 @@ for k in keys:
     joints_export.iloc[:, 1::3] = joints_export.iloc[:, 1::3]*-1
     joints_export.iloc[:, 2::3] = joints_export.iloc[:, 2::3]*-1
     
-    hipCenter = joints_export.loc[:][['Hip.R_x', 'Hip.R_y', 'Hip.R_z',
+    cam = np.asarray(item['theta'][0,:3])  # s, tx, ty
+    cam_scales = cam[0]
+    cam_transl = cam[1:]
+    
+    proc_params = item['proc_param']
+    img_size = proc_params['target_size']  # processed image size
+    start_pt = proc_params['start_pt']
+    inv_proc_scale = 1./np.asarray(proc_params['scale'])
+    bbox = proc_params['bbox']  # bbox is obtained from OpenPose: bbox here is (cx, cy, scale, x, y, h, w)
+    principal_pt = np.array([img_size, img_size]) / 2.
+    flength = 500.
+    tz = flength / (0.5 * img_size * cam_scales)
+    trans = np.hstack([cam_transl, tz])  # camera translation vector ??
+    final_principal_pt = (principal_pt + start_pt) * inv_proc_scale
+    
+    
+    hips = joints_export[['Hip.R_x', 'Hip.R_y', 'Hip.R_z',
                                       'Hip.L_x', 'Hip.L_y', 'Hip.L_z']]
 
-    joints_export['hip.Center_x'] = hipCenter.iloc[0][::3].sum()/2
-    joints_export['hip.Center_y'] = hipCenter.iloc[0][1::3].sum()/2
-    joints_export['hip.Center_z'] = hipCenter.iloc[0][2::3].sum()/2
+    # hips[::3] += trans[0]
+    # hips[1::3] += trans[1]
+    # hips[2::3] += trans[2]
+    
+    # joints_export[['Hip.R_x', 'Hip.R_y', 'Hip.R_z', 'Hip.L_x', 'Hip.L_y', 'Hip.L_z']] = hips
+
+    joints_export['hip.Center_x'] = hips.iloc[0][::3].sum()/2
+    joints_export['hip.Center_y'] = hips.iloc[0][1::3].sum()/2
+    joints_export['hip.Center_z'] = hips.iloc[0][2::3].sum()/2
     
     os.makedirs("bvh/csv/", exist_ok=True)
     joints_export.to_csv("bvh/csv/%06d.jpg.csv" % k)
